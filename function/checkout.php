c13ef7eeeeb989d908675132e5f7e6471cfb82cb
Yu Hui <florance118@gmail.com>
2025-04-08 02:07:38 +0800

    checkout page still progressing... payment not finished

---
+++ function/checkout.php
@@ -0,0 +1,14 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["selected_items"])) {
+    $selectedItems = $_POST['selected_items'];
+    $names = $_POST['item_names'];
+    $prices = $_POST['item_prices'];
+    $quantities = $_POST['item_quantities'];
+    $subtotals = $_POST['item_subtotals'];
+}
+else {
+    $_SESSION['errorToast'] = "Invalid Request!";
+    header("Location: homepage.php");
+    return;
+}
+?>
