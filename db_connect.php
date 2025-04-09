021832b7b583e311291c3a3d3dc11898c719af3f
kexinnnnnnn7 <ongkx-pm23@student.tarc.edu.my>
2025-04-09 20:14:30 +0800

    add db connection php

---
+++ db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$host = "192.168.192.73";       
+$username = "nbuser";        
+$password = "abc12345";           
+$database = "cloud";  
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully!";
+?>
